aa8e1509f92dd5fded21e6b48ed70035125115c1
lgarci78 <74846976+lgarci78@users.noreply.github.com>
2021-05-13 23:53:50 -0700

    Character movement done

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -12,29 +12,40 @@ class Tutorial extends Phaser.Scene {
         console.log("in Tutorial");
         this.physics.world.gravity.y = -10;
 
-        // let group = this.physics.add.group({
-        //     defaultKey: 'dart',
-        //     bounceY: 0.25,
-        //     collideWorldBounds: true
-        // });
         up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
 
-        // let p1 = group.create(320, 400).setGravity(0, 0);    
         this.p2 = this.physics.add.sprite(100, 500, 'dart', 0); 
         this.p2.body.collideWorldBounds = true;  
 
-        //group.create(500, 500).setGravity(0, 100);
     }
     
     update(){
 
+
+        // movement from https://www.html5gamedevs.com/topic/7774-apply-force-to-sprite/
+
         if(Phaser.Input.Keyboard.JustDown(right)){
-            this.p2.x +=50;
-            //this.p2.applyForce(50);
+            console.log(Math.cos(0) * 100)
+            this.p2.body.velocity.x = Math.cos(0) * 100;
         }
+
+        if(Phaser.Input.Keyboard.JustDown(left)){
+            this.p2.body.velocity.x = Math.cos(180) * 100;
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(up)){
+            this.p2.body.velocity.y = Math.cos(90) * 100;
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(down)){
+            this.p2.body.velocity.y = Math.cos(0) * 100;
+        }
+
+
+
     }
 }
