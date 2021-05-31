2cb2f0efa75afb22a0d239c48709504a7b60d115
lgarci78 <74846976+lgarci78@users.noreply.github.com>
2021-05-30 21:56:31 -0700

    updating main and tutorial

--- src/main.js
+++ src/main.js
@@ -14,9 +14,39 @@ let config = {
     scene: [Title, Tutorial, Level1, Level2]
 }
 
+let health = 100;
+let healthDisplay;
+let healthConfig = {
+    fontFamily: 'Courier',
+    fontSize: '16px',
+    backgroundColor: '#8DC63F',
+    color: '#FFFFFF',
+    align: 'right',
+    padding:{
+        top: 5,
+        bottom: 5,
+    },
+    fixedWidth: 110
+}
+
+let endConfig = {
+    fontFamily: 'Courier',
+    fontSize: '16px',
+    backgroundColor: '#8DC63F',
+    color: '#FFFFFF',
+    align: 'right',
+    padding:{
+        top: 5,
+        bottom: 5,
+    },
+    fixedWidth: 200
+}
+
 let game = new Phaser.Game(config);
 let up, down, left, right, pressed;
 let keyRIGHT, R;
 let backgroundMusic;
 let bubbles;
+let endDisplay;
+let minushealth;
 let col = false;
--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -7,6 +7,7 @@ class Tutorial extends Phaser.Scene {
     preload(){
         this.load.image('dart', './assets/img/Dart.png');
         this.load.image('tileset', './assets/img/underwatertm.png');
+        this.load.image('bubble' , './assets/img/Bubble.png');
         this.load.tilemapTiledJSON('tilemap', './assets/img/tutorialmap.json');
         this.load.atlas('player', './assets/img/DiverSprite.png', './assets/img/DiverSprite.json');
         this.load.image('restart', './assets/img/RestartText.png');
@@ -20,9 +21,11 @@ class Tutorial extends Phaser.Scene {
         const tileset = map.addTilesetImage('underwater', 'tileset');
 
         const bgLayer = map.createLayer('Background', tileset, 0, 0);
+
         const terrainLayer = map.createLayer('Collidable', tileset, 0, 0);
-        const chest = map.createLayer('Chest', tileset, 0, 0);
         const extras = map.createLayer('Extra', tileset, 0, 0);
+
+        const chest = map.createLayer('Chest', tileset, 0, 0);
 
         terrainLayer.setCollisionByProperty({
             Collision: true
@@ -32,6 +35,19 @@ class Tutorial extends Phaser.Scene {
             Collision: true
         });
 
+        //Health Display
+        healthDisplay = this.add.text(265, 15, "Health: " + health, healthConfig);
+
+        minushealth = setInterval(mhealth, 1000);
+
+        function mhealth(){
+            console.log("In here");
+            if(health > 0){
+                health-= 10;
+            }
+            healthDisplay.text = "Health:  " + health;
+        }
+
         console.log("in Tutorial");
         this.physics.world.gravity.y = -10;
 
@@ -40,9 +56,11 @@ class Tutorial extends Phaser.Scene {
         left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-        //this.p2 = this.physics.add.sprite(100, 300, 'dart', 0); 
         this.p2 = this.physics.add.sprite(100, 250, 'player', 0);
         this.p2.body.collideWorldBounds = true;  
+
+        //BUBBLES
+        this.bubble1 = this.add.sprite(350, 300, 'bubble', 0);
 
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         this.cameras.main.startFollow(this.p2, true, 0.3, 0.3);
@@ -51,11 +69,10 @@ class Tutorial extends Phaser.Scene {
         this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
         this.physics.add.collider(this.p2, terrainLayer);
 
-
         this.physics.add.collider(this.p2, chest, function(p2, chest){
             console.log("Got to Chest!");
             col = true;
-        });  
+        });
 
         //animation
         var frameNames = this.textures.get('player').getFrameNames();
@@ -132,41 +149,112 @@ class Tutorial extends Phaser.Scene {
     }
     
     update(){
-
+        let gameOver = false;
+        let movement = true;
+        if(health == 0){
+            gameOver = true;
+            movement = false;
+            this.p2.body.velocity.x = 0;
+            this.p2.body.velocity.y = 0;
+            health = 0;
+            healthDisplay.text = "Health: " + health;
+            endDisplay = this.add.text(100, 250, "Press R to Restart!", endConfig);
+            //this.scene.pause("tutorialScene");
+            this.game.sound.stopAll();
+        }
 
         if (col){
             col = false;
             this.scene.start('level1Scene');
         }
 
+        if(this.checkCollision(this.p2, this.bubble1)){
+            this.bubble1.destroy();
+            health = 100;
+            healthDisplay.text = "Health: " + health;
+        }
 
         // movement from https://www.html5gamedevs.com/topic/7774-apply-force-to-sprite/
 
-        if(Phaser.Input.Keyboard.JustDown(right)){
-            console.log(Math.cos(0) * 100)
-            this.p2.body.velocity.x = Math.cos(0) * 100;
+        if(movement && Phaser.Input.Keyboard.JustDown(right)){
+            /*
+            console.log("Right Velocity: ")
+            console.log(this.p2.body.velocity.x)
+            
+            if(this.p2.body.velocity.x < -50){
+                this.p2.body.velocity.x *= 0.5;
+            }
+
+            else if(this.p2.body.velocity.x >= -50 && this.p2.body.velocity.x < -20){
+                this.p2.body.velocity.x *= 0.15;
+            }
+            */
+            //else if(this.p2.body.velocity.x >= -20){
+                this.p2.body.velocity.x = Math.cos(0) * 100;
+            //}
+
             this.p2.play("swimRight");
+
+            
         }
 
-        if(Phaser.Input.Keyboard.JustDown(left)){
-            this.p2.body.velocity.x = Math.cos(180) * 100;
+        if(movement && Phaser.Input.Keyboard.JustDown(left)){
+            /*
+            console.log("Left Velocity: ")
+            console.log(this.p2.body.velocity.x)
+            
+            if(this.p2.body.velocity.x > 50){
+                this.p2.body.velocity.x *= 0.5;
+            }
+
+            else if(this.p2.body.velocity.x <= 50 && this.p2.body.velocity.x > 20){
+                this.p2.body.velocity.x *= 0.25;
+            }
+            */
+            //else if(this.p2.body.velocity.x <= 20){
+                this.p2.body.velocity.x = Math.cos(180) * 100;
+            //}
+
+            //this.p2.body.velocity.x = -100;
             this.p2.play("swimLeft");
         }
 
-        if(Phaser.Input.Keyboard.JustDown(up)){
+        if(movement && Phaser.Input.Keyboard.JustDown(up)){
+            //console.log("Up Velocity: ")
+            //console.log(this.p2.body.velocity.y)
             this.p2.body.velocity.y = Math.cos(90) * 100;
         }
 
-        if(Phaser.Input.Keyboard.JustDown(down)){
+        if(movement && Phaser.Input.Keyboard.JustDown(down)){
+            //console.log("Down Velocity: ")
+            //console.log(this.p2.body.velocity.y)
             this.p2.body.velocity.y = Math.cos(0) * 100;
         }
 
 
-        if (Phaser.Input.Keyboard.JustDown(R)) {
+        if (gameOver && Phaser.Input.Keyboard.JustDown(R)) {
+            console.log("In R");
             backgroundMusic.stop();
             bubbles.stop();
             this.sound.play('sfx_select');
-            this.scene.start('titleScene'); 
+            health = 100;
+            endDisplay.destroy();
+            clearInterval(minushealth);
+            this.scene.restart(); 
+            
+        }
+    }
+
+    checkCollision(player, bubble){
+        // simple AABB checking
+        if( player.x < bubble.x + bubble.width && 
+            player.x + player.width > bubble.x &&
+            player.y < bubble.y + bubble.height &&
+            player.height + player.y > bubble.y){
+                return true
+        }
+        else{
+            return false;
         }
     }
 }
